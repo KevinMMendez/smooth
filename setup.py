fc0c4735771f900e31384dfc7efee302ed11977a
Kevin Mendez <35716119+KevinMMendez@users.noreply.github.com>
2020-02-19 11:45:48 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name="smooth",
+    version="0.1.0",
+    description="Data approximation using a cubic smoothing spline",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    license="MIT License",
+    url="https://github.com/kevinmmendez/smooth",
+    packages=["smooth"],
+    python_requires='>=3.5',
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scipy"],
+    author='Kevin Mendez, David Broadhurst',
+    author_email='k.mendez@ecu.edu.au, d.broadhurst@ecu.edu.au',
+)
---
+++ smooth/__init__.py
@@ -0,0 +1,5 @@
+from .__version__ import version as __version__
+
+from .smooth import CubicSmoothSpline
+
+__all__ = ["CubicSmoothSpline"]
